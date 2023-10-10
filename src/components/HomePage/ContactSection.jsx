b3647a4746b7ecd63a095b6961ea37378c1e6bf7
Alexandar Valov <alexvulov123@gmail.com>
2023-10-10 17:56:29 +0300

    Made the Landing Page fully responsive.

--- src/components/HomePage/ContactSection.jsx
+++ src/components/HomePage/ContactSection.jsx
@@ -7,7 +7,7 @@ const ContactSection = () => {
   return (
     <div className="w-full bg-[#f8f8f8] mt-12 pb-20 flex flex-col justify-center items-center">
       <div className="w-full max-w-[1140px] px-4 py-20 mb-2 flex justify-between items-center gap-8 border-b-2 border-gray-300">
-        <div className="text-4xl font-medium">
+        <div className="text-3xl sm:text-4xl font-medium">
           Let's work
           <br />
           together
@@ -15,14 +15,16 @@ const ContactSection = () => {
         <div
           onMouseOver={mouseOverEvent}
           onMouseOut={mouseOutEvent}
-          className="bg-black hover:bg-white text-white hover:text-black text-sm text-center font-medium w-24 h-24 rounded-full flex justify-center items-center hover:scale-110 hover:border-black hover:border-2 duration-300 ease-in-out"
+          className="bg-black hover:bg-white text-white hover:text-black text-xs sm:text-sm text-center font-medium w-20 sm:w-24 h-20 sm:h-24 rounded-full flex justify-center items-center hover:scale-110 hover:border-black hover:border-2 duration-300 ease-in-out"
         >
-          Write an Email
+          Write an
+          <br />
+          Email
         </div>
       </div>
-      <div className="w-full max-w-[1140px] px-4 flex justify-between items-center text-sm text-gray-500">
-        <div>© 2023 All Rights Reserved</div>
-        <div>Designed and developed by me</div>
+      <div className="w-full max-w-[1140px] px-4 flex justify-between items-center text-xs sm:text-sm text-gray-500">
+        <div className="text-left">© 2023 All Rights Reserved</div>
+        <div className="text-right">Designed and developed by me</div>
       </div>
     </div>
   );
--- src/components/HomePage/LandingPage.jsx
+++ src/components/HomePage/LandingPage.jsx
@@ -8,20 +8,20 @@ const LandingPage = () => {
   const { mouseOverEvent, mouseOutEvent } = useCursorContext();
 
   return (
-    <div className="w-full relative max-w-[1140px] m-auto px-4 py-24">
-      <div className="w-full h-[30rem] sm465:h-96 absolute top-10 sm465:top-28 left-0 flex flex-col px-10 sm465:px-20 justify-center items-center">
-        <div className="w-full flex justify-center items-center animate-iconsAnimation1">
+    <div className="w-full relative max-w-[1140px] m-auto px-4 py-0 sm308:py-12 sm:py-24">
+      <div className="w-full h-[22rem] sm308:h-80 sm600:h-96 absolute -top-20 sm308:top-16 sm465:top-20 sm600:top-12 sm:top-24 left-0 flex flex-col justify-between items-center">
+        <div className="w-full flex justify-center items-center animate-iconsAnimation1 px-20">
           <Link
             to="/"
             title="Gmail"
             onMouseOver={mouseOverEvent}
             onMouseOut={mouseOutEvent}
-            className="w-14 h-14 bg-red-200 rounded-full flex justify-center items-center hover:scale-125 duration-300 ease-in-out"
+            className="w-8 sm308:w-10 sm465:w-12 h-8 sm308:h-10 sm465:h-12 bg-red-200 rounded-full flex justify-center items-center hover:scale-125 duration-300 ease-in-out"
           >
             <SiGmail size={22} className="text-red-500" />
           </Link>
         </div>
-        <div className="w-full flex justify-end items-center mt-24 sm:mt-32 mb-12 sm465:mb-28 sm:mb-20 animate-iconsAnimation2">
+        <div className="w-full flex justify-end items-center animate-iconsAnimation2 px-4 sm465:px-20">
           <Link
             to="https://github.com/JustAlex-WebDev"
             target="_blank"
@@ -29,12 +29,12 @@ const LandingPage = () => {
             title="GitHub"
             onMouseOver={mouseOverEvent}
             onMouseOut={mouseOutEvent}
-            className="w-14 h-14 bg-gray-200 rounded-full flex justify-center items-center hover:scale-125 duration-300 ease-in-out"
+            className="w-8 sm308:w-10 sm465:w-12 h-8 sm308:h-10 sm465:h-12 bg-gray-200 rounded-full flex justify-center items-center hover:scale-125 duration-300 ease-in-out"
           >
             <FaGithub size={22} className="text-gray-500" />
           </Link>
         </div>
-        <div className="w-full flex ml-20 sm465:ml-40 sm575:mt-20 md:mt-0 justify-center md:justify-start items-center animate-iconsAnimation3">
+        <div className="w-full flex justify-start items-center animate-iconsAnimation3 px-4 sm465:px-20">
           <Link
             to="https://www.linkedin.com/in/alexandar-valov-667567242/"
             target="_blank"
@@ -42,18 +42,18 @@ const LandingPage = () => {
             title="LinkedIn"
             onMouseOver={mouseOverEvent}
             onMouseOut={mouseOutEvent}
-            className="w-14 h-14 bg-blue-200 flex justify-center items-center rounded-full hover:scale-125 duration-300 ease-in-out"
+            className="w-8 sm308:w-10 sm465:w-12 h-8 sm308:h-10 sm465:h-12 bg-blue-200 flex justify-center items-center rounded-full hover:scale-125 duration-300 ease-in-out"
           >
             <FaLinkedin size={22} className="text-blue-500" />
           </Link>
         </div>
       </div>
-      <div className="w-full md:w-3/4 relative m-auto mt-24 font-medium flex flex-col gap-6 justify-center items-left -z-10">
-        <div className="text-sm sm575:text-base">Alexandar Valov</div>
-        <div className="text-4xl sm465:text-5xl sm575:text-6xl xmd:text-7xl">
+      <div className="w-full max-w-[800px] m-auto mt-24 font-medium flex flex-col gap-6 justify-center items-left">
+        <div className="text-text-sm sm600:text-base">Alexandar Valov</div>
+        <div className="text-4xl sm465:text-5xl sm600:text-6xl lg850:text-7xl">
           Front-End
         </div>
-        <div className="text-4xl sm465:text-5xl sm575:text-6xl xmd:text-7xl w-[85%]  md:text-end">
+        <div className="text-4xl sm465:text-5xl sm600:text-6xl lg850:text-7xl pl-0 sm:pl-28">
           Web Developer
         </div>
       </div>
